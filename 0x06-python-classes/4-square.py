be6ce8626fbd4cd6a2f3e3828ccdbd7d21292b32
Gasoremugwaneza <mugwanezagasore073@gmail.com>
2022-12-20 10:39:57 -0800

    question 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -9,7 +9,7 @@ class Square:
     """ updating a privare instance attribute """
     def __init__(self, size=0):
         """ instantiation of class square """
-        self.__size = size
+        self.size = size
     
     @property
     def size(self):
