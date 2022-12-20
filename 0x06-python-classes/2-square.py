d86b1a39bcc551f6220a5170da088c1440d051a7
Gasoremugwaneza <mugwanezagasore073@gmail.com>
2022-12-20 09:56:55 -0800

    question 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# a class that define a square
+"""
+    we are not allowed to import any module
+"""
+
+
+class Square:
+    """ a class that define square """
+    def __init__(self, size=0):
+        """ instantiation of square class """
+        self__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
